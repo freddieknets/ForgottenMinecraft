2f2e5164368fc3e179f715ab4c77a3043e16b902
freddieknets <frederikvanderveken@gmail.com>
2022-01-10 13:04:33 +0100

    Stop tracking logs, and moved server location to /srv

--- StartDevServer.sh
+++ StartDevServer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-path=/home/frederik/minecraftadmin
+path=/srv/minecraftadmin
 cd $path
-pm2 start --name minecraftadmin -l ${path}/Logs/fulloutput.log -o ${path}/Logs/output.log -e ${path}/Logs/errors.log npm -- run dev
+pm2 start --name minecraftadmin -l ${path}/logs/fulloutput.log -o ${path}/logs/output.log -e ${path}/logs/errors.log npm -- run dev
 pm2 save
 read  -n 1 -p "Copy-paste the following output (hit any key to continue):"
 pm2 startup systemd
